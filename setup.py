dee07a17f2516194435720162fc42704f7e95e8e
Célio Vieira de Magalhães Junior <celio.junior@grupo-sei.net>
2025-07-22 16:12:42 -0300

    feat: implementa ciclo de vida com disconnect seguro na IQ Option

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""Setup personalizado da API da IQ Option."""
+from setuptools import setup, find_packages
+
+setup(
+    name="iqoptionapi",
+    version="0.0.2",
+    packages=find_packages(),
+    install_requires=[
+        "pylint",
+        "requests",
+        "websocket-client"
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    description="API personalizada da IQ Option",
+    long_description="Fork da API do Rafael Faria adaptado por Célio Junior",
+    author="Célio Junior",
+    author_email="profissional.celiojunior@gmail.com",
+    url="https://github.com/celiovmjr/iqoptionapi",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
